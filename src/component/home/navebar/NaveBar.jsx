103de8c8cce790eecad669edfc37ce8b6cb71cb5
rokib26 <salehmahmud6465@gmail.com>
2023-11-30 16:50:27 +0600

    toggle manu

--- src/component/home/navebar/NaveBar.jsx
+++ src/component/home/navebar/NaveBar.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./navebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const NaveBar = () => {
   const [naveScroll, setNaveScroll] = useState(false);
+  const [showNave, setShowNave] = useState(false);
 
   const handelScroll = () => {
     if (window.scrollY > 0) {
@@ -19,19 +22,36 @@ const NaveBar = () => {
     };
   }, []);
 
+  const toggolNave = () => {
+    setShowNave(true);
+  };
+
+  const closeManu = () => {
+    setShowNave(false);
+  };
+
   return (
-   <>
-    <div className={`navber ${naveScroll ? "scrolled" : ""}`}>
-      <div className="nave-bar">
+    <>
+      <div className={`navber ${naveScroll ? "scrolled" : ""}`}>
+        <div className="nave-bar">
           <div className="logo">
-            <h2><i>image</i></h2>
+            <h2>
+              <i>image</i>
+            </h2>
           </div>
+        </div>
       </div>
-    </div>
-    <div className="manu">
-        
-    </div>
-   </>
+
+      <div className={`menu ${showNave ? "show" : ""}`}>
+        <FontAwesomeIcon icon={faTimes} onClick={closeManu} />
+      </div>
+
+      <div className="manu">
+        <div className="manu-icon">
+          <FontAwesomeIcon icon={faBars} onClick={toggolNave} />
+        </div>
+      </div>
+    </>
   );
 };
 
